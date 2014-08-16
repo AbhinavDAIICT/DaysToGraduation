0d7aa10290274c9d5e0a8ca07caec25de9b2ed68
AbhinavDAIICT <abhinavtripathi01@hotmail.com>
2014-08-16 22:24:18 +0530

    code related to daystograduation.com
    This is the first commit and contains the index page with basic looks

---
+++ index.php
@@ -0,0 +1,220 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Days To Graduation</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+<!--     Custom styles for this template 
+    <link href="navbar-fixed-top.css" rel="stylesheet">
+-->
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <style type="text/css">
+		
+	  #avatar {
+  border-radius:         50%;
+  -moz-border-radius:    50%;
+  -webkit-border-radius: 50%;
+  background: #eeeeee no-repeat;
+  background-size: 128px 128px;
+	text-align:center;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;  
+    margin-top:10%; }
+    
+    #titletext{
+    display:block;
+        margin-left: auto;
+    margin-right: auto;
+    margin-top:auto;
+
+    font-size: 26px;
+    font-family: 'Open Sans Condensed';
+    color : #fcfcfc;
+  }
+  
+  #formtext{
+    font-size:15px;
+    color : #ffffff;
+    font-family: 'Tahoma';
+    text-align : justify;
+	font-weight:bold;
+  }
+
+  #paramail{
+    font-size:25px;
+    color : #ffffff;
+    font-family: 'Tahoma';
+    text-align : center;
+	font-weight:bold;
+  }
+
+	.auto-style1 {
+		margin-right: -15px;
+		margin-left: -15px;
+    font-size:15px;
+    color : #000000;
+    font-family: 'Tahoma';
+	}
+
+	.tilefooter {
+    font-size : 22px;
+    color : #f8981d;
+    font-family: 'Arvo';
+    font-weight: bold;
+    text-align: center;
+
+	}
+
+	</style>
+	<!-- Add this script -->
+	<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5182e5f42beb3460"></script>
+  </head>
+
+  <body>
+
+    <!-- Fixed navbar 
+    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Project name</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+              <ul class="dropdown-menu" role="menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li class="divider"></li>
+                <li class="dropdown-header">Nav header</li>
+                <li><a href="#">Separated link</a></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="../navbar/">Default</a></li>
+            <li><a href="../navbar-static-top/">Static top</a></li>
+            <li class="active"><a href="./">Fixed top</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+  <!--    </div>
+    </div>
+-->
+    <div class="container">
+	    <!-- Main Row with respect to which content area and offset is determined -->
+	    <div class="row">
+	    	<!-- Content Row -Its divided in Logo Area and Content Area -->
+		    <div class="col-xs-offset-2 col-md-8" style="background-color:white; height:700px">
+		        <!-- Logo Row - This is the area where logo and Subscription link goes -->
+			    <div class="row" style="background-color:white;">
+			    	<div class="col-md-4" style="height:252px;background-color:#f8981d;padding:5px 5px 5px 5px;border:solid 3px white">
+			    	<img id="avatar" alt="logo" height="128" src="images/logo.png" width="128"><br><p id="titletext" style="text-align:center"><b>Days To Graduation</b></p>
+			    	</div>	
+			    	<div class="col-md-8" style="height:252px;background-color:#0f1257;padding:5px 5px 5px 5px;border:solid 3px white">
+			    	<div class="row" style="background:#0f1257;min-height:130px;padding:5px 5px 5px 5px;margin:5px 5px 5px 5px;"><p id="formtext">Subscribe now to recieve our Edition Zero instantly.
+For Edition Zero, read a very personal letter written by a student to his professor discussing his romantic and academic life.</p><p id="paramail">Email Address  <input type="text" class="form-control" placeholder=""></p><p style="text-align:center"><button class="btn btn-warning btn-lg" type="button">
+   Subscribe
+  </button></p></div>
+			    	
+			   		</div>	
+		    	</div><!-- Logo Row close -->
+		        <!-- Content Area -This is the area where intro and other stuff goes -->
+			    <div class="row" style="background-color:#fcfcfc;">
+				    <div class="auto-style1" style="padding:5px 5px 5px 5px;margin:5px 5px 5px 5px;">
+						Days To Graduation is a meaningful newsletter for 
+						college students and graduates. <br> <br>You read about 
+						interesting people all the time, but when you realize 
+						that they are the same age as yours and are going to 
+						college every morning, it really hits you. Nothing is 
+						impossible. Everything out there is in your reach. <br> <br>
+						Get heartfelt stories from across campuses, widen your 
+						horizons and change your perspective. See how it is to 
+						be in someone else's shoes.<br><br>In our weekly newsletter, 
+						you'll find meaningful and informative articles about 
+						things that matter in your life. Be ready for the world 
+						outside. Learn from each other.<br><br>College is a 
+						beautiful journey, and you don't have to make it alone. 
+						We aim to generate empathy between all the people in the 
+						world.<br><br>Why should the university that you go to limit 
+						your world? <br> <br>Subscribe to Days To Graduation 
+						newsletter now get a heartfelt and meaningful newsletter 
+						right in your inbox! </div>
+				    <div class="row" style="padding:5px 5px 5px 5px;margin:5px 5px 5px 5px;">
+					    <div class="col-md-4" style="background:#DCEBEE;min-height:186px;border:solid 1px white;padding:5px 5px 5px 5px;">
+					    	<p class="tilefooter">Inspiring Stories from Campuses</p>
+							<p>Read about interesting and 
+							inspiring things people like you are doing in their 
+							college lives both on/off their campuses.</p></div>
+					    <div class="col-md-4" style="background:#DCEBEE;min-height:186px;border:solid 1px white;padding:5px 5px 5px 5px;">
+					    	<p class="tilefooter">A Day After Graduation</p><p>Read about students who 
+							decided to take the road less travelled after 
+							graduation, and how a single day in their lives is, 
+							in their words.</p></div>
+					    <div class="col-md-4" style="background:#DCEBEE;min-height:186px;border:solid 1px white;padding:5px 5px 5px 5px;">
+					    	<p class="tilefooter">Changed Me and How</p><p>Read a heartfelt confession 
+							about how books, music or travel changed somebody's 
+							life in college.</p><br></div>
+				    </div>
+				    <div class="row" style="padding:5px 5px 5px 5px;margin:5px 5px 5px 5px;">
+					    <div class="col-md-4" style="background:#DCEBEE;min-height:186px;border:solid 1px white;padding:5px 5px 5px 5px;">
+					    	<p class="tilefooter">A Day in a Different College</p><p>Ever wondered how a day in a 
+							college in a different continent is like? Step in 
+							someone else's shoes for a day. Live multiple lives. 
+							Expand your campus life.</p></div>
+					    <div class="col-md-4" style="background:#DCEBEE;min-height:186px;border:solid 1px white;padding:5px 5px 5px 5px;">
+					    	<p class="tilefooter">Only What Matters</p><p>There are no quick bite 
+							sized snacks here. You only get gratifying meals 
+							that will take some time to digest.</p><br></div>
+					    <div class="col-md-4" style="background:#DCEBEE;min-height:186px;border:solid 1px white;padding:5px 5px 5px 5px;">
+					    	<p class="tilefooter">Share Your Story</p><p>Share your story with us. 
+							Connect with students all over the world. Inspire, 
+							start a conversation and reach out. Get Email 
+							replies. You don't have to be alone.</p></div>
+				    </div>
+			    	
+		    	</div><!-- Content Area close -->
+		    </div><!-- Content Row close -->
+	    </div><!-- Main Row close -->
+	
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Go to www.addthis.com/dashboard to customize your tools -->
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5182e5f42beb3460"></script>
+
+  </body>
+</html>
